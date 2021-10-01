32c24e905ee053e350e99ff1be1b2ea3266101c6
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-10-01 16:24:45 +0530

    Given 3 String Find The Maximum

---
+++ UC3/UC2MaxString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Assignment
+{
+    class UC2MaxString
+    {
+        public static string MaxString(string firstS, string secondS, string thirdS)
+        {
+            if (firstS.CompareTo(secondS) > 0 && firstS.CompareTo(thirdS) > 0 ||
+                firstS.CompareTo(secondS) >= 0 && firstS.CompareTo(thirdS) > 0 ||
+                firstS.CompareTo(secondS) > 0 && firstS.CompareTo(thirdS) >= 0)
+            {
+                return firstS;
+            }
+
+            if (secondS.CompareTo(firstS) > 0 && secondS.CompareTo(thirdS) > 0 ||
+                secondS.CompareTo(firstS) >= 0 && secondS.CompareTo(thirdS) > 0 ||
+                secondS.CompareTo(firstS) > 0 && secondS.CompareTo(thirdS) >= 0)
+            {
+                return secondS;
+            }
+
+            if (thirdS.CompareTo(firstS) > 0 && thirdS.CompareTo(secondS) > 0 ||
+                thirdS.CompareTo(firstS) >= 0 && thirdS.CompareTo(secondS) > 0 ||
+                thirdS.CompareTo(firstS) > 0 && thirdS.CompareTo(secondS) >= 0)
+            {
+                return thirdS;
+            }
+
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+    }
+}
